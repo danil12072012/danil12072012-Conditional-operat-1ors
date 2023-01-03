09ded31c9b656ac259097a591b61e7628a95a781
mycomputer <Danil12.07.2012@gail.com>
2023-01-03 13:35:34 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека " + age + " он совершеннолетний.");
+        }
 
+        if (age < 18) {
+            System.out.println("Если возраст человека " + age + " он не достиг совершеннолетия, нужно немного подождать.");
+        }
 
     }
 }
